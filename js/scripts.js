e7df73152a8ebd7421a6cbfdb22cbcfe9b802c96
Kai Thompson <kaihrthompson@gmail.com>
2025-03-23 16:43:41 -0400

    tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,60 @@ const sauceTypeElem = document.getElementById('sauceType');
 const toppingsElem = document.querySelectorAll('.toppings');
 const pizzaSizeElem = document.getElementById('pizzaSize');
 const pizzaShapeElem = document.getElementById('pizzaShape');
+const sauceAmountElem = document.getElementById('sauceAmount');
+const cheeseAmountElem = document.getElementById('cheeseAmount');
+
+const sauceAmountDisplayElem = document.getElementById('sauceAmountDisplay');
+const cheeseAmountDisplayElem = document.getElementById('cheeseAmountDisplay');
 
 const studentId = document.getElementById('studentId');
 
 const orderSummary = document.getElementById('orderSummary');
 const pizzaImage = document.getElementById('pizzaImage');
 const orderButton = document.getElementById('order');
+
 orderButton.addEventListener('click', function(event) {
     event.preventDefault();
     let newPizza = getPizza();
     console.log(newPizza.pizzaSummary());
+    orderSummary.textContent = newPizza.pizzaSummary();
     GeneratePizzaImage(newPizza);
 });
 
 studentId.addEventListener('click', function() {
     studentId.textContent = 'Student ID: 1284787';
+});
+
+sauceAmountElem.addEventListener('change', function() 
+{
+    let sauceAmount = sauceAmountElem.value;
+    
+    if(sauceAmount <= 3) {
+        sauceAmountDisplayElem.textContent = 'Light (' + sauceAmount + ')';
+    }
+    else if(sauceAmount <= 7) {
+        sauceAmountDisplayElem.textContent = 'Normal (' + sauceAmount + ')';
+    }
+    else if(sauceAmount <= 10) {
+        sauceAmountDisplayElem.textContent = 'Extra (' + sauceAmount + ')';
+    }
+    
+});
+
+cheeseAmountElem.addEventListener('change', function()
+{
+    let cheeseAmount = cheeseAmountElem.value;
+    
+    if(cheeseAmount <= 3) {
+        cheeseAmountDisplayElem.textContent = 'Light (' + cheeseAmount + ')';
+    }
+    else if(cheeseAmount <= 7) {
+        cheeseAmountDisplayElem.textContent = 'Normal (' + cheeseAmount + ')';
+    }
+    else if(cheeseAmount <= 10) {
+        cheeseAmountDisplayElem.textContent = 'Extra (' + cheeseAmount + ')';
+    }
+    
 });
 
 
@@ -57,6 +96,8 @@ function getPizza()
     let sauceType = sauceTypeElem.value;
     let size = pizzaSizeElem.value;
     let shape = pizzaShapeElem.value;
+    let sauceAmount = sauceAmountElem.value;
+    let cheeseAmount = cheeseAmountElem.value;
 
     let toppings = [];
     toppingsElem.forEach(topping => {
@@ -65,7 +106,7 @@ function getPizza()
         }
     });
 
-    let pizza1 = new pizza(sauceType, toppings, size, shape);
+    let pizza1 = new pizza(sauceType, toppings, size, shape, sauceAmount, cheeseAmount);
     return pizza1;
 }
 
@@ -76,24 +117,70 @@ class pizza {
     toppings;
     size;
     shape;
-    constructor(sauceType, toppings, size, shape) {
+    sauceAmount;
+    cheeseAmount;
+
+    constructor(sauceType, toppings, size, shape, sauceAmount, cheeseAmount) {
         this.sauceType = sauceType;
         this.toppings = toppings;
         this.size = size;
         this.shape = shape;
+        this.sauceAmount = sauceAmount;
+        this.cheeseAmount = cheeseAmount;
     }
 
     pizzaSummary()
     {
-        let summary = `Pizza Summary\n`;
-        summary += `Sauce Type: ${this.sauceType}\n`;
-        summary += `Toppings:\n`;
-        this.toppings.forEach(topping => {
-            summary += `Toppings: ${topping}\n`;
-        });
-        summary += `Size: ${this.size}\n`;
-        summary += `Shape: ${this.shape}\n`;
+        let summary = `Thank you for your order!`;
+        summary += ` You ordered a ${this.size} ${this.shape} pizza with ${this.getSauceAmount()} ${this.sauceType} sauce and ${this.getCheeseAmount()} cheese.`;
+        summary += ` You also added ${this.toppings.length} toppings which `;
+        
+        if(this.toppings.length == 1)
+        {
+            summary += 'was ' + this.toppings[0] + '.';
+            return summary;
+        }
+        else {
+            summary += 'were ';
+        }
+        for(let i = 0; i < this.toppings.length; i++)
+        {
+            if(i == this.toppings.length - 1)
+            {
+                summary += `and ${this.toppings[i]}.`;
+            }
+            else
+            {
+                summary += `${this.toppings[i]}, `;
+            }
+        }
         return summary;
+    }
+
+    getSauceAmount()
+    {
+        if(this.sauceAmount <= 3) {
+            return 'Light';
+        }
+        else if(this.sauceAmount <= 7) {
+            return 'Normal';
+        }
+        else if(this.sauceAmount <= 10) {
+            return 'Extra';
+        }
+    }
+
+    getCheeseAmount()
+    {
+        if(this.cheeseAmount <= 3) {
+            return 'Light';
+        }
+        else if(this.cheeseAmount <= 7) {
+            return 'Normal';
+        }
+        else if(this.cheeseAmount <= 10) {
+            return 'Extra';
+        }
     }
 
 }
@@ -163,6 +250,20 @@ function GeneratePizzaImage(pizza)
             pizzaCheese.appendChild(toppingDiv);
         }
     });
+
+    pizzaSauce.style.height = ((parseInt(pizza.sauceAmount) + 85) + '%');
+    pizzaSauce.style.width = ((parseInt(pizza.sauceAmount) + 85) + '%');
+
+    pizzaSauce.style.top = ((15 - parseInt(pizza.sauceAmount))/2 + '%');
+    pizzaSauce.style.left = ((15 - parseInt(pizza.sauceAmount))/2 + '%');
+
+    pizzaCheese.style.height = ((parseInt(pizza.cheeseAmount) + 85) + '%');
+    pizzaCheese.style.width = ((parseInt(pizza.cheeseAmount) + 85) + '%');
+
+    pizzaCheese.style.top = ((15 - parseInt(pizza.cheeseAmount))/2 + '%');
+    pizzaCheese.style.left = ((15 - parseInt(pizza.cheeseAmount))/2 + '%');
+
+
 
 
     pizzaImage.appendChild(pizzaBase);
